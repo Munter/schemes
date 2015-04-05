857e7ffc6594783d17e4aa30b52d2464e96c9d05
Peter Müller <munter@fumle.dk>
2015-04-05 21:05:31 +0200

    Add missing unofficial schemes to allByName list

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ data.unofficial = require('./lib/unofficial.json').filter(function (item) {
   return !allByName[item.scheme];
 });
 
+data.unofficial.forEach(function (schemeObj) {
+  allByName[schemeObj.scheme] = extend(schemeObj, { type: 'unofficial' });
+});
+
 
 module.exports = extend(data, { allByName: allByName });
