d9e029ce4bb5e11d19754994dd8b52c0d61b8cdf
Peter Müller <munter@fumle.dk>
2015-04-05 20:23:28 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var extend = require('extend');
+
+var data = {
+  permanent: require('./lib/iana-permanent.json'),
+  provosional: require('./lib/iana-provisional.json'),
+  historical: require('./lib/iana-historical.json')
+};
+
+var allByName = {};
+
+Object.keys(data).forEach(function (type) {
+  data[type].forEach(function (schemeObj) {
+    allByName[schemeObj.scheme] = extend(schemeObj, { type: type });
+  });
+});
+
+data.unofficial = require('./lib/unofficial.json').filter(function (item) {
+  return !allByName[item.scheme];
+});
+
+
+module.exports = extend(data, { allByName: allByName });
