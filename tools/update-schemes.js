4e109d42cb936760a1087720fb65bd709d73009a
Peter Müller <munter@fumle.dk>
2016-03-29 17:20:29 +0200

    Updated build script for new xml layout

--- tools/update-schemes.js
+++ tools/update-schemes.js
@@ -12,61 +12,64 @@ unofficial.forEach(function (item) {
   unofficialMap[item.scheme] = item;
 });
 
-var sections = {
-  'permanent': '#uri-schemes-1',
-  'provisional': '#uri-schemes-2',
-  'historical': '#uri-schemes-3'
-};
+var sections = {};
 
-get.concat('http://www.iana.org/assignments/uri-schemes/uri-schemes.xml', function (err, data) {
+get.concat('http://www.iana.org/assignments/uri-schemes/uri-schemes.xml', function (err, res, data) {
   if (err) {
     throw err;
   }
 
   var $ = cheerio.load(data.toString());
 
+  $('record').each(function (idx, el) {
+    var status = $(el).find('status').text().toLowerCase();
+
+    if (!sections[status]) {
+      sections[status] = [];
+    }
+
+    var result = {
+      scheme: $(el).find('value').text(),
+      description: $(el).find('description').text(),
+      reference: Array.prototype.map.call($(el).find('xref:not([type="person"])'), function (el) {
+        var type = $(el).attr('type');
+        var href;
+
+        switch (type) {
+        case 'rfc':
+        case 'draft':
+          href = 'http://www.iana.org/go/' + $(el).attr('data');
+          break;
+        case 'registry':
+          href = 'http://www.iana.org/assignments/' + $(el).attr('data');
+          break;
+        default:
+        }
+
+        return {
+          type: type,
+          href: href
+        };
+      })
+    };
+
+    if ($(el).find('file[type="template"]').length === 1) {
+      result.template = 'http://www.iana.org/assignments/uri-schemes/' + $(el).find('file[type="template"]').text();
+    }
+
+    if (unofficialMap[result.scheme]) {
+      console.error(chalk.yellow('WARN: Duplicate scheme in unofficial schemes list: ' + result.scheme));
+      console.error(chalk.yellow('\t Already exists in iana ' + status + '\n'));
+    }
+
+    sections[status].push(result);
+  });
+
+  // Write out the results
   Object.keys(sections).forEach(function (key) {
-    var data = Array.prototype.map.call($(sections[key] + ' record'), function (el) {
-      var result = {
-        scheme: $(el).find('value').text(),
-        description: $(el).find('description').text(),
-        reference: Array.prototype.map.call($(el).find('xref:not([type="person"])'), function (el) {
-          var type = $(el).attr('type');
-          var href;
-
-          switch (type) {
-          case 'rfc':
-          case 'draft':
-            href = 'http://www.iana.org/go/' + $(el).attr('data');
-            break;
-          case 'registry':
-            href = 'http://www.iana.org/assignments/' + $(el).attr('data');
-            break;
-          default:
-          }
-
-          return {
-            type: type,
-            href: href
-          };
-        })
-      };
-
-      if ($(el).find('file[type="template"]').length === 1) {
-        result.template = 'http://www.iana.org/assignments/uri-schemes/' + $(el).find('file[type="template"]').text();
-      }
-
-      if (unofficialMap[result.scheme]) {
-        console.error(chalk.yellow('WARN: Duplicate scheme in unofficial schemes list: ' + result.scheme));
-        console.error(chalk.yellow('\t Already exists in iana ' + key + '\n'));
-      }
-
-      return result;
-    });
-
     var fileName = Path.join('lib', 'iana-' + key) + '.json';
 
-    fs.writeFile(fileName, JSON.stringify(data, undefined, 2), function (err) {
+    fs.writeFile(fileName, JSON.stringify(sections[key], undefined, 2), function (err) {
       if (err) {
         throw err;
       }
@@ -74,4 +77,5 @@ get.concat('http://www.iana.org/assignments/uri-schemes/uri-schemes.xml', functi
       console.log('Wrote ' + fileName + ': ' + data.length + ' entries');
     });
   });
+
 });
